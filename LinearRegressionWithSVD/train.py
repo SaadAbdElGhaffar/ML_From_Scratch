39b7d155ed9bb9274c0014a87c8f4dd36f6bf690
SaadAbdElGhaffar <saadabdelghaffar639@gmail.com>
2024-08-05 05:11:52 +0300

    Forget something

--- LinearRegressionWithSVD/train.py
+++ LinearRegressionWithSVD/train.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn import datasets
 import matplotlib.pyplot as plt
+from Ridge import RidgeCustom
 
 X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
